7c3b7c44b25179ac0588cafecfabcf502874ee5d
phantomxxi <haianhlol2001@gmail.com>
2020-06-24 22:09:35 +0700

    first commit

---
+++ JStest.js
@@ -0,0 +1,18 @@
+// // Chờ tài liệu tải xong
+// $(document).ready(function() {
+//     // Lệnh trong đây chỉ chạy khi tài liệu đã tạo xong
+//     var btb = document.getElementById('btbChangeImage');
+//     btb.onclick = ChangeImage;
+// });
+
+// var CurrentImage = 1;
+
+// function ChangeImage() {
+//     if (CurrentImage == 1) {
+//         $('#hinh').attr("src", "./Screenshot (1270).png");
+//         CurrentImage = 2;
+//     } else {
+//         $('#hinh').attr("src", "./Screenshot (1267).png");
+//         CurrentImage = 1;
+//     }
+// }
